2e86a548a24a4382e5abc7780eaca71f5bbaef5f
Evileudo Matias Bezerra <Evileudobezerra0040@digitalcollege.internal>
2025-04-23 16:26:03 -0300

    update

--- semana.js
+++ semana.js
@@ -1,1 +1,30 @@
-let dia = 1
+const readline = require("readline-sync");
+
+let dia = parseInt(readline.question('qual dia da semana?'))
+
+switch (dia) {
+    case 1 :
+        console.log('domingo')
+        break
+    case 2 :
+        console.log('segunda')
+        break
+    case 3 :
+        console.log('terça')
+        break
+    case 4 :
+        console.log('quarta')
+        break
+    case 5 :
+        console.log('quinta')
+        break
+    case 6 :
+        console.log('sexta')
+        break
+    case 7 :
+        console.log('sabado')
+        break
+
+    default:
+        console.log('dia nao encontrado')
+}
