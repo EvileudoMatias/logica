799f5484125cf809d762fbc965610942e7c5f4c3
Evileudo Matias Bezerra <Evileudobezerra0040@digitalcollege.internal>
2025-04-23 17:04:08 -0300

    update

---
+++ imc.js
@@ -0,0 +1,19 @@
+const readline = require("readline-sync");
+
+let peso = Number(readline.question("qual seu peso? "));
+let altura = Number(readline.question("qual sua altura? "));
+let resultado = peso/(altura*altura);
+console.log('Seu imc:' + " "+ resultado + " ")
+
+if (resultado <= 18){
+    console.log('abaixo do peso')
+}
+else if (resultado >= 18 && resultado <= 24){
+    console.log('Parabens! Peso normal')
+}
+else if (resultado >= 24 && resultado <=40){
+    console.log('Cuidado!! Acima do peso')
+}
+else {
+    console.log('Cuidado!! Obesidade morbida')
+}
