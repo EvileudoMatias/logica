1a6234d602edabf39119d6011d464b70e992dc1f
Evileudo Matias Bezerra <Evileudobezerra0040@digitalcollege.internal>
2025-04-23 16:01:48 -0300

    muita mudanca

--- cinema.js
+++ cinema.js
@@ -1,7 +1,16 @@
-let idade = 3;
-if(idade >= 16) {
-    console.log (idade + " pode entrar")
-}
-else {
-    console.log(idade + " nao pode entrar")
-}
+const readline = require("readline-sync");
+
+let idade = parseInt(readline.question("Qual sua idade? "));
+let estudante = readline.question("Voce é estudante ou idoso? (s/n)");
+
+if (idade >= 16) {
+    console.log ("entrada permitida");
+    if (estudante.toLowerCase() === "s") {
+        console.log("voce tem direito a meia entrada");
+    } 
+    else {
+        console.log("voce paga o valor integral");
+    }
+} else {
+    console.log("Entrada nao permitida para menores de 16 anos");
+}   
